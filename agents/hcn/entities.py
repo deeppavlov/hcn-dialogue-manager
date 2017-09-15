5a310ad674348f9959de660ef3bf185a6617095c
Mary Vikhreva <mary.vikhreva@gmail.com>
2017-09-15 14:58:04 +0300

    fix: fix entities.py dependencies

--- agents/hcn/entities.py
+++ agents/hcn/entities.py
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from enum import Enum
+from enum import Enum, auto
 import numpy as np
 
 
