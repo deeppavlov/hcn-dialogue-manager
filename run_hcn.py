a081b23e46867badf62a9493f4cfb01a55cdea17
Mary Vikhreva <mary.vikhreva@gmail.com>
2017-09-18 16:04:50 +0300

    feat: add bash script to execute basic training

---
+++ run_hcn.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# display data example
+python3 ./utils/display_data.py -t dialog_babi:task:6 -n 10
+
+# build directory
+mkdir -p ./build
+
+# train for 24 epochs
+python3 ./utils/train_model.py -t dialog_babi:task:5\
+                               -m hcn.agents.hcn.hcn:HybridCodeNetworkAgent\
+                               -mf ./build/hcn\
+                               --datatype train:ordered\
+                               --num-epochs 24\
+                               --log-every-n-secs -1\
+                               --log-every-n-epochs -1\
+                               --learning-rate .1\
+                               --hidden-dim 128\
+                               --validation-every-n-epochs 5
+
